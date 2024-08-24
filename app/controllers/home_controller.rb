746197685ceb53f6e7539f8db257436b73d38fb2
MAEDA AYUMU <>
2024-08-24 18:59:01 +0900

    aboutページを作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @title = 'デイトラ'
-    render 'home/index'
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
+  get '/about' => 'home#about'
 end
