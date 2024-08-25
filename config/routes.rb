00665a23913ee3be13ad04bbbf3c10ccceab9f87
MAEDA AYUMU <>
2024-08-25 13:05:59 +0900

    homeへの遷移リンクを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get '/' => 'home#index'
+  root to: 'home#index' #先のように書くことでHelperを使えるようになる。
+  # get '/' => 'home#index' 上記に変更する。
+  # 特に指定しなければhomeのindexを表示という意味になる。
   get '/about' => 'home#about'
 end
