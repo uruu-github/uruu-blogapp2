54f957bb47a362884912234cf83d4194b590b51b
MAEDA AYUMU <>
2024-08-27 08:21:11 +0900

    showアクション作成に伴う　URL作成、Viewの作成、コントローラーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-    # このように書くことでHelper(root_path)を使えるようになる。
-    # get '/' => 'home#index' を上記に変更する。
-    # 特に指定しなければhomeのindexを表示という意味になる。
+  resources :articles, only: [:show]
 end
