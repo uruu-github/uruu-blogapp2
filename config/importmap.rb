7828504001f941c8f9454a5feb49a31fa22a8b2f
MAEDA AYUMU <>
2024-09-01 09:51:25 +0900

    １．rails sが立ち上がらないためbootsnapをコメントアウト ２．deleteメソッドがうまくいかなかったので修正した。 　・rails ujsをimportmapにとりこみ 　・link_toの記述を変更 　・Turboとrails-ujsをimport

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "@rails/ujs", to: "rails-ujs.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
