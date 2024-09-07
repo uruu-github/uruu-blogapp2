7da260eea881d64ddc5c23f03b46a14fad07b0ac
MAEDA AYUMU <>
2024-09-08 08:49:09 +0900

    articleテーブルにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_06_231126) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_07_232758) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
