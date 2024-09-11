dceb6b4f1234aa947c1f09730d4ecc4858c8d7a1
MAEDA AYUMU <>
2024-09-12 08:11:00 +0900

    birthday,gender,introductionの設定 delegateを使った簡略化

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,13 +25,27 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  delegate :birthday, :gender, :introduction, :subscribed, to: :profile, allow_nil: true
+
   def has_written?(article)
     articles.exists?(id: article.id)
   end
 
   def display_name
-    self.email.split('@').first
+    profile&.nickname || self.email.split('@').first
   end
+
+  # def birthday
+  #   profile&.birthday
+  # end
+
+  # def gender
+  #   profile&.gender
+  # end
+
+  # def introduction
+  #   profile&.introduction
+  # end
 
   def prepare_profile
     profile || build_profile
