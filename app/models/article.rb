68234295eec5de27ef32fa2a63e0b654de04ebbb
MAEDA AYUMU <>
2024-09-15 15:36:40 +0900

    eyecatchの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,6 +14,8 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
+  has_one_attached :eyecatch
+
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 } #2文字以上~100文字以下
   validates :title, format: { with: /\A(?!@)/ }
