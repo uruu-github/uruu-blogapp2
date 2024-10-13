9aba88d707552af752fc9907b3297229dc60d4e1
MAEDA AYUMU <>
2024-10-13 17:51:18 +0900

    no message

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
-// app/javascript/application.js
-
 import "@hotwired/turbo-rails"; // Turboをインポート
-import "trix"; // Trixエディタをインポート
+import "https://cdnjs.cloudflare.com/ajax/libs/trix/2.1.6/trix.min.js"; // TrixをCDNからインポート
+import "https://cdnjs.cloudflare.com/ajax/libs/trix/2.1.6/trix.css"; // TrixのCSSをCDNからインポート
 import "@rails/actiontext"; // ActionTextをインポート
