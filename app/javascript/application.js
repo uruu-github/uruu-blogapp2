1d1e13edf2d05affb83c017c49b7d0d80452d6b5
MAEDA AYUMU <>
2024-10-13 15:13:25 +0900

    no message

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
+// app/javascript/application.js
+import "@hotwired/turbo-rails"
+import "@hotwired/stimulus"
 import "trix"
 import "@rails/actiontext"
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,7 @@
 pin "application"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.10
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.10
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.2.100
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
