6391c8fb2717fbe156fc6db4a90107cc7f811de0
MAEDA AYUMU <>
2024-10-13 15:49:17 +0900

    no message

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 pin "application" # アプリケーションのエントリーポイント
 pin "trix", to: "https://unpkg.com/trix@1.3.1/dist/trix.js"
-pin "trix/dist/trix.css", to: "https://unpkg.com/trix@1.3.1/dist/trix.css"
+# pin "trix/dist/trix.css", to: "https://unpkg.com/trix@1.3.1/dist/trix.css"
 pin "@rails/actiontext", to: "actiontext.esm.js" # ActionTextをピン
 pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # Turbo Railsをピン
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # Stimulusをピン
