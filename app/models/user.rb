3ed081179ecfb5f071692ac24690c97d199e4f60
MAEDA AYUMU <>
2024-10-14 12:18:25 +0900

    tips3,4

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,9 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, :introduction, :subscribed, to: :profile, allow_nil: true
@@ -59,6 +62,11 @@ class User < ApplicationRecord
 
   def follow!(user)
     following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by(following_id: user.id)
+    relation.destroy!
   end
 
   def prepare_profile
