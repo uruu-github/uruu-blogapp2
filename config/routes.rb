1b5e2509859ecebfadab5604c961aa217ac01800
MAEDA AYUMU <>
2024-10-15 10:20:06 +0900

    tip3　アンフォロー機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
+    resources :unfollows, only: [:create]
   end
 
   #プロフィールはユーザーに対して複数ないので単数形でOK。
