5bce7ff4c52f294c15d8a86ba1fa99ec42b5419d
MAEDA AYUMU <>
2024-10-15 10:48:21 +0900

    00　初期設定（routes,controller,view,遷移リンク）

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'articles#index'
+  resource :timeline, only: [:show]
 
   resources :articles do
     resources :comments, only: [:new, :create]
